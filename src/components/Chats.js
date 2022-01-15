fbd3858ed79818d750a76b7a4294cc128c258878
Kenneth Adjei <[kmadjei25@gmail.com]>
2022-01-15 04:16:34 -0500

    completing authContext tutorial video

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -3,11 +3,37 @@ import { useHistory } from 'react-router-dom';
 import { ChatEngine } from 'react-chat-engine';
 import { auth } from '../firebase';
 
+import { useAuth } from '../contexts/AuthContext';
+
 const Chats = () => {
+    const history = useHistory();
+    // getting user details from context into CHats.js component
+    const { user } = useAuth();
+
+
+    const handleLogout = async () => {
+        await auth.signOut();
+
+        history.push('/')
+    }
     return ( 
-        
-        <h1>CHats</h1>
-        <div ></div>
+        <div className='chats-page'>
+            <div className="nav-bar">
+                <div className="logo-tab">
+                    Unichat
+                </div>
+                <div onClick={handleLogout} className="logout-tab">
+                    Logout
+                </div>
+
+                <ChatEngine 
+                    height="calc(100vh-66px)"
+                    projectId='18953b06-8a97-4230-a7d2-c140e76a9ce5'
+                    userName='.'
+                    userSecret='.'
+                />
+            </div>
+        </div>
      );
 }
  
